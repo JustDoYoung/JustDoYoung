cd8c1433c2f26045d2bad29d4b1ab4d772be34a8
Young Kim <guckka94@gmail.com>
2024-06-04 20:05:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,13 @@ markdown_text = """
 수학을 좋아하고 그래픽 분야에 관심이 있습니다.   
 ![Unity](https://img.shields.io/badge/unity-%23000000.svg?style=plastic&logo=unity&logoColor=white) 와 ![Unreal Engine](https://img.shields.io/badge/unrealengine-%23313131.svg?style=plastic&logo=unrealengine&logoColor=white) 을 다룰 수 있습니다.
    
-📞 Contact : guckka94@gmail.com   
-✒️ Blog : https://gukka94.tistory.com   
-
-   
 ### 🌱 Things I code with ...
   ![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=plastic&logo=c%2B%2B&logoColor=white)
   ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=plastic&logo=csharp&logoColor=white)
   ![Unity](https://img.shields.io/badge/unity-%23000000.svg?style=plastic&logo=unity&logoColor=white) 
   ![Unreal Engine](https://img.shields.io/badge/unrealengine-%23313131.svg?style=plastic&logo=unrealengine&logoColor=white) 
-   
+
+##### 📞 Contact : guckka94@gmail.com ✒️ Blog : https://gukka94.tistory.com   
    
 ### 😎 Online Judge
 ![solvedac badge](https://solvedac-readme-badge.vercel.app/api/v1/badge?user=tornado0310&theme=github-dark&compact=1)
