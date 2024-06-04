713881643e20d28aeb86df78f871d60d2c69a3dd
Young Kim <guckka94@gmail.com>
2024-06-04 20:44:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MAX_POST = 3
 
 markdown_text = """
 # <img src="https://github.com/JustDoYoung/JustDoYoung/assets/63029463/312b6a4a-8df0-4ae7-9a19-42dc5d5e3a7b" width="30" height="30"/> Nice to meet you! 
+##### 📞 Contact : guckka94@gmail.com ✒️ Blog : https://gukka94.tistory.com   
 
 저는 클라이언트 개발자 **DoYoung** 입니다.  
 수학을 좋아하고 그래픽 분야에 관심이 있습니다.   
@@ -16,8 +17,6 @@ markdown_text = """
   ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=plastic&logo=csharp&logoColor=white)
   ![Unity](https://img.shields.io/badge/unity-%23000000.svg?style=plastic&logo=unity&logoColor=white) 
   ![Unreal Engine](https://img.shields.io/badge/unrealengine-%23313131.svg?style=plastic&logo=unrealengine&logoColor=white) 
-
-##### 📞 Contact : guckka94@gmail.com ✒️ Blog : https://gukka94.tistory.com   
    
 ### 😎 Online Judge
 ![solvedac badge](https://solvedac-readme-badge.vercel.app/api/v1/badge?user=tornado0310&theme=github-dark&compact=1)
