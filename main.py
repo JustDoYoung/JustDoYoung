f4715518db6e6f58ca34a59fc9a97f86605f1d17
Young Kim <guckka94@gmail.com>
2024-06-04 18:33:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     else:
        
         feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br>"
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br>\n"
         
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
